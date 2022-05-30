b3b8040534a92c4c947e1b470997f82a076bd23b
durid-ah <ahmad.durid96@gmail.com>
2022-05-29 22:30:17 -0600

    added letter parser

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
-import { ParserFn, sequenceOf, str } from "./parser";
+import { letter, sequenceOf, str } from "./parser";
 
 
-const run = (parser: ParserFn, target: string) => {
-   const initial = {
-      target,
-      index: 0,
-      result: null,
-      isError: false
-   }
-   return parser(initial);
-} 
 
 const parser = str('hello there!');
-console.log(run(parser, 'hello there!'));
-console.log(run(parser, 'This is not correct'));
+console.log(parser.run('hello there!'));
+console.log(parser.run('This is not correct'));
 
 const parser_2 = sequenceOf([str('hello there!'), str('goodbye there!')]);
-console.log(run(parser_2, 'hello there!goodbye there!'));
-console.log(run(parser_2, 'hello there!goodbye there'));
-console.log(run(parser_2, ''));
+console.log(parser_2.run('hello there!goodbye there!'));
+console.log(parser_2.run('hello there!goodbye there'));
+console.log(parser_2.run(''));
+
+const parser_3 = parser.map(res => {
+   let resString = res as string;
+   return resString.toUpperCase();
+});
+console.log(parser_3.run('hello there!'));
+
+const parser_4 = parser_2
+   .mapError((_, idx) => `Expected a greeting @ index ${idx}`);
+console.log(parser_4.run(''));
+
+const parser_5 = letter;
+console.log(parser_5.run('1234'));
+console.log(parser_5.run('asde'));
---
+++ src/parser/letter.parser.ts
@@ -0,0 +1,19 @@
+import { updateError, updateState } from "./parser.helper";
+import { Parser, ParserState } from "./parser.model";
+
+const lettersRegex = /^[A-Za-z]+/;
+export const letter = new Parser(
+   (state: ParserState): ParserState => {
+      const {index, target, isError} = state;
+      if (isError) return state;
+         
+      const slicedTarget = target.slice(index);
+      if (slicedTarget.length === 0)
+         return updateError(state, `letter: Unexpected end of input`)
+      
+      const regexMatch = slicedTarget.match(lettersRegex);
+      if (regexMatch) 
+         return updateState(state, index + regexMatch.length, regexMatch[0]);
+         
+      return updateError(state, `letter: Couldn't match letters at index ${index}`);
+   });
