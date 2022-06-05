e31b6ea359f65dbd13a4d61157a1ee2ce8cec294
durid-ah <ahmad.durid96@gmail.com>
2022-06-05 00:13:56 -0600

    fixes

--- src/parser/many-one.parser.ts
+++ src/parser/many-one.parser.ts
@@ -1,27 +1,29 @@
 import { Parser, State, updateError, updateResult } from ".";
 
 /** Match one or more instances of the parser */
-export const manyOne = (parser: Parser) => new Parser((state: State): State => {
-   if (state.isError) return state;
-   
-   let next = state;
-   const results = [];
-   let done = false;
+export function manyOne(parser: Parser) {
+   return new Parser((state: State): State => {
+      if (state.isError) return state;
+      
+      let next = state;
+      const results = [];
+      let done = false;
 
-   while(!done) {
-      next = parser.parserStateTransfromerFn(next);
-      if (!next.isError) {
-         results.push(next.result);
-      } else {
-         done = true;
+      while(!done) {
+         next = parser.parserStateTransfromerFn(next);
+         if (!next.isError) {
+            results.push(next.result);
+         } else {
+            done = true;
+         }
       }
-   }
 
-   if (results.length === 0) {
-      return updateError(
-         state, 
-         `manyOne: Unable to match any input using parser @ index`);
-   }
+      if (results.length === 0) {
+         return updateError(
+            state, 
+            `manyOne: Unable to match any input using parser @ index`);
+      }
 
-   return updateResult(next, results);
-})
+      return updateResult(next, results);
+   });
+}
