ed621a7daad9a50bd773b91bcf2aaa35571a8772
durid-ah <ahmad.durid96@gmail.com>
2022-06-09 08:12:33 -0600

    fix up the letters parser with the latest type changes

--- src/parser/letters.parser.ts
+++ src/parser/letters.parser.ts
@@ -1,18 +1,27 @@
-import { updateError, updateState, Parser, State } from ".";
+import { ERR_RESULT, Parser, State } from "./models/parser.model";
+import { updateError, updateState } from "./models/parsers.helper";
+import { newOne } from "./models/result-type.model";
+import { newErr, newOk } from "./models/result.model";
 
 const lettersRegex = /^[A-Za-z]+/;
 export const letters = new Parser<string>(
    (state: State<any>): State<string> => {
-      const {index, target, isError} = state;
-      if (isError) return state;
+      const {index, target, result} = state;
+      if (result.resType === ERR_RESULT) return state;
          
       const slicedTarget = target.slice(index);
       if (slicedTarget.length === 0)
-         return updateError(state, `letter: Unexpected end of input`)
+      {
+         const err = newErr(`letter: Unexpected end of input`);
+         return updateError(state, err);
+      }
       
       const regexMatch = slicedTarget.match(lettersRegex);
-      if (regexMatch) 
-         return updateState<string>(state, index + regexMatch[0].length, regexMatch[0]);
+      if (regexMatch) {
+         const res = newOk(newOne(regexMatch[0])); 
+         return updateState<string>(state, index + regexMatch[0].length, res);
+      } 
 
-      return updateError(state, `letter: Couldn't match letters at index ${index}`);
+      const err = newErr(`letter: Couldn't match letters at index ${index}`);
+      return updateError(state, err);
    });
