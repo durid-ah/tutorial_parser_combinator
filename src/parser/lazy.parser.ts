e73ad8ff571033c975bd8d565d08ebe9b22133d7
durid-ah <ahmad.durid96@gmail.com>
2022-06-09 21:33:34 -0600

    fixed up the lazy parser

--- src/parser/lazy.parser.ts
+++ src/parser/lazy.parser.ts
@@ -1,8 +1,8 @@
-import { Parser, Thunk, Result, State } from ".";
+import { Parser, Thunk, State } from ".";
 
-export function lazy<R = Result>(parserThunk: Thunk<R>): Parser<R> { 
-   return new Parser<R>(
-      (state: State<any>): State<R> => {
+export function lazy<T = string, R = string, E = string>(parserThunk: Thunk<T, R, E>): Parser<T, R, E> { 
+   return new Parser<T, R, E>(
+      (state: State<T, E>): State<R, E> => {
          const parser = parserThunk();
          return parser.parserStateTransfromerFn(state);
       }
