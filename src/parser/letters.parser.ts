80cd508d15fa24f76c99ae129f55d84dfa007467
durid-ah <ahmad.durid96@gmail.com>
2022-06-09 21:23:50 -0600

    finishing touches on the letters parser

--- src/parser/letters.parser.ts
+++ src/parser/letters.parser.ts
@@ -1,27 +1,32 @@
 import { ERR_RESULT, Parser, State } from "./models/parser.model";
-import { updateError, updateState } from "./models/parsers.helper";
 import { newOne } from "./models/result-type.model";
-import { newErr, newOk } from "./models/result.model";
+import { mapErr, newErr, newOk } from "./models/result.model";
 
 const lettersRegex = /^[A-Za-z]+/;
-export const letters = new Parser<string>(
-   (state: State<any>): State<string> => {
-      const {index, target, result} = state;
-      if (result.resType === ERR_RESULT) return state;
-         
-      const slicedTarget = target.slice(index);
-      if (slicedTarget.length === 0)
-      {
-         const err = newErr(`letter: Unexpected end of input`);
-         return updateError(state, err);
-      }
-      
-      const regexMatch = slicedTarget.match(lettersRegex);
-      if (regexMatch) {
-         const res = newOk(newOne(regexMatch[0])); 
-         return updateState<string>(state, index + regexMatch[0].length, res);
-      } 
+export function letters<T>(){ 
+   return new Parser<T>(
+      (state: State<T>): State<string> => {
+         const {index, target, result} = state;
+         if (result.resType === ERR_RESULT) return mapErr(state);
 
-      const err = newErr(`letter: Couldn't match letters at index ${index}`);
-      return updateError(state, err);
-   });
+         const slicedTarget = target.slice(index);
+         if (slicedTarget.length === 0)
+         {
+            const err = newErr(`letter: Unexpected end of input`);
+            return {...state, result: err};
+         }
+
+         const regexMatch = slicedTarget.match(lettersRegex);
+         if (regexMatch) {
+            const res = newOk(newOne(regexMatch[0])); 
+            return {
+               ...state,
+               index: index + regexMatch[0].length, 
+               result: res
+            };
+         } 
+
+         const err = newErr(`letter: Couldn't match letters at index ${index}`);
+         return {...state, result: err};
+      });
+}
