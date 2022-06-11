b10da28f8005588c3a40acddcdbababafb719ae2
durid-ah <ahmad.durid96@gmail.com>
2022-06-11 01:16:08 -0600

    moved the sep-by parser to the collection parsers folder and fixed up the type

---
+++ src/parser/collection_parsers/sep-by.parser.ts
@@ -0,0 +1,46 @@
+import { ERR_RESULT, Parser, State } from "../models/parser.model";
+
+/**
+ * Create the parser that will match values between a specified parsed value
+ * @param separator The parser that will match the separator value(s)
+ * @returns 
+ */
+export function sepBy<T = string, S = string, R = string>(separator: Parser<T,S>) { 
+   return (value: Parser<T,R>) => new Parser<T,R>(
+      (state: State<T>): State<R> => { 
+         const results: R[] = [];
+         let nextState = state;
+
+         while(true) {
+            const thingWeWantState = value
+               .parserStateTransfromerFn(nextState);
+            
+            if (thingWeWantState.result.resType === ERR_RESULT) 
+               break;
+
+            if (thingWeWantState.result.result.resType === 'one')
+               results.push(thingWeWantState.result.result.value);
+            else
+               results.push(...thingWeWantState.result.result.value);
+
+            nextState = {...thingWeWantState, result: null};
+
+            const separatorState = separator
+               .parserStateTransfromerFn({...nextState});
+
+            if (separatorState.result.resType === ERR_RESULT) 
+               break;
+
+            nextState = { ...separatorState, result: null };
+         }
+
+         return {
+            ...nextState, 
+            result: {
+               resType: 'ok',
+               result: { resType: 'many', value: results}
+            }
+         };
+      }
+   );
+}
