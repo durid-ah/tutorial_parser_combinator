c84503f0f694a15bb5816c9109778ef202ef6a5b
durid-ah <ahmad.durid96@gmail.com>
2022-06-16 22:08:58 -0600

    final touches to the between parser to use the target type

--- src/parser/between.parser.ts
+++ src/parser/between.parser.ts
@@ -2,11 +2,11 @@ import { Parser, sequenceOf } from ".";
 import { newMany } from "./models/result-cardinal.model";
 import { newOk } from "./models/result.model";
 
-export function between<T, R, E>(left: Parser<T, R, E>, right: Parser<T,R,E>) {
-   return (content: Parser<T, R, E>) => 
-      sequenceOf<T, R, E>([left, content, right])
-         .map<R>(res => {
-            const resArr = res.result.value as R[];
+export function between<R1, R2, T, E>(left: Parser<R1,R2,T,E>, right: Parser<R1,R2,T,E>) {
+   return (content: Parser<R1, R2, T, E>) => 
+      sequenceOf<R1, R2, T, E>([left, content, right])
+         .map<R2>(res => {
+            const resArr = res.result.value as R2[];
             const betweenRes = [...resArr.slice(1, -1)]
             return newOk(newMany(betweenRes));
          });
