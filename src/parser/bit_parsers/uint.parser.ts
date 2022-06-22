b36affa0a198c48b549079c5cb6fa7f8a872f3fd
durid-ah <ahmad.durid96@gmail.com>
2022-06-21 21:48:50 -0600

    polishing the uint parser and bumped up the target library

--- src/parser/bit_parsers/uint.parser.ts
+++ src/parser/bit_parsers/uint.parser.ts
@@ -4,11 +4,22 @@ import { newOk } from "../models/result.model";
 import { Bit } from "./bit.parser";
 
 
+/**
+ * Parses a `DataView` into an unsigned integer
+ * @param bitCount the number of bits the number will have
+ */
 export function UInt(bitCount: number) {
+   if (bitCount < 1)
+      throw new Error(`UInt: n must be larger than 0, but we got ${bitCount}`);
+
+   if (bitCount > 32)
+      throw new Error(`UInt: n must be less than 32, but we got ${bitCount}`);
+
+
    return SequenceOf(Array.from({length: bitCount}, () => Bit()))
       .map<number>(bits => {
          const mappedResult = (bits.result.value as number[])
-            .reduce((acc, bit, i) => acc + (bit << (bitCount - 1 -i)), 0);
+            .reduce((acc, bit, i) => acc + Number(BigInt(bit) << BigInt(bitCount - 1 -i)), 0);
          
          return newOk(newOne(mappedResult));
       });
