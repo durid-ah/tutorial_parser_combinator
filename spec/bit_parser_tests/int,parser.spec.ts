1fddce4796a241a6f221a15ce5c0bcddba92d7d4
durid-ah <ahmad.durid96@gmail.com>
2022-06-26 22:00:23 -0600

    build up the int parser  and its test

---
+++ spec/bit_parser_tests/int,parser.spec.ts
@@ -0,0 +1,15 @@
+import { Many, SequenceOf } from "../../src/parser";
+import { Int } from "../../src/parser/bit_parsers/int.parser";
+import { ResOk } from "../../src/parser/models/result.model";
+
+
+describe('Int Parser Tests', () => {
+   const parser = Int(8);
+
+   it('Should parse every 8 bits in the DataView', () =>{
+      const data = new DataView((new Uint8Array([234, 128])).buffer);
+      
+      const value = (SequenceOf([parser]).run(data).result as ResOk<number>).result.value[0];
+      expect(value).toEqual(-22);
+   });
+});
--- src/parser/bit_parsers/int.parser.ts
+++ src/parser/bit_parsers/int.parser.ts
@@ -1,6 +1,32 @@
-import { Parser } from "../models/parser.model";
+import { SequenceOf } from "../collection_parsers/sequence-of.parser";
+import { newOne } from "../models/result-cardinal.model";
+import { newOk } from "../models/result.model";
+import { Bit } from "./bit.parser";
 
 
 export function Int(bitCount: number) {
-   return new Parser();
+   if (bitCount < 1)
+   throw new Error(`UInt: n must be larger than 0, but we got ${bitCount}`);
+
+   if (bitCount > 32)
+      throw new Error(`UInt: n must be less than 32, but we got ${bitCount}`);
+
+   return SequenceOf(Array.from({length: bitCount}, () => Bit()))
+      .map<number>(bits => {
+
+         if (bits[0] === 0) {
+            const mappedResult = (bits.result.value as number[])
+               .reduce((acc, bit, i) => acc + Number(BigInt(bit) << BigInt(bitCount - 1 -i)), 0);
+            
+            return newOk(newOne(mappedResult));
+         }
+
+         else {
+            const mappedResult = -(1 + (bits.result.value as number[])
+            .reduce((acc, bit, i) => acc + Number(BigInt(bit === 0? 1: 0) << BigInt(bitCount - 1 -i)), 0));
+         
+            return newOk(newOne(mappedResult));
+         }
+
+   });
 }
